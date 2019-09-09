cabf607d22c5d6f55e40be588bdb760e26c47635
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-09-08 22:52:42 -0300

    Funções de submenu adicionadas

--- menu.c
+++ menu.c
@@ -1,55 +1,279 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <windows.h>
+//#include <windows.h>
+#include <locale.h>
 
 void showFuncionarios(){
-    printf("\n Funcionarios");
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Funcionarios ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Funcionarios");
+    printf("\n (2) Adicionar funcionario");
+    printf("\n (3) Editar funcionario \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    }
 }
 
 void showClientes(){
-    printf("\n Clientes");
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Clientes ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Clientes");
+    printf("\n (2) Adicionar Clientes");
+    printf("\n (3) Editar Clientes \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    }
 }
 
 void showProdutos(){
-    printf("\n Produtos");
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Produtos ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Produtos");
+    printf("\n (2) Adicionar Produtos");
+    printf("\n (3) Editar Produtos \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    }
 }
 
 void showIngredientes(){
-    printf("\n Ingredientes");
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Ingredientes ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Ingredientes");
+    printf("\n (2) Adicionar Ingredientes");
+    printf("\n (3) Editar Ingredientes \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    }
 }
 
 void showPedidos(){
-    printf("\n Contabilizacao");;
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Pedidos ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Pedidos");
+    printf("\n (2) Adicionar Pedidos");
+    printf("\n (3) Editar Pedidos \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    }
 }
 
 void showReclamacoes(){
-    printf("\n Reclamacoes");;
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Reclamações ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Reclamações");
+    printf("\n (2) Adicionar Reclamações");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    }
 }
 
 void showPromocoes(){
-    printf("\n Promocoes");;
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Promoções ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
+    printf("\n (1) Vizualizar Promoções");
+    printf("\n (2) Adicionar Promoções");
+    printf("\n (3) Editar Promoções \n");
+    printf("\n (3) Excluir Promoções \n");
+
+    scanf("%d", &resposta);
+
+    switch (resposta)
+    {
+    case 1:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Vizualizar");
+        break;
+    case 2:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Adicionar");
+        break;
+    case 3:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Editar");
+        break;
+    case 4:
+        printf("\e[H\e[2J"); //Limpa a tela
+        printf("\n Excluir");
+        break;
+    }
 }
 
 void showRelatorios(){
-    printf("\n Relatorios");;
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Relatorios ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
 }
 
 void showFechamento(){
-    printf("\n Fechamento");;
+    int resposta;
+
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    printf("\n ==============================================");
+    printf("\n ============ Fechamento ==============");
+    printf("\n ==============================================");
+    printf("\n \n");
+
 }
 
 
 int main()
 {
+    //Define o padrão UTF-8
+    setlocale(LC_ALL, "Portuguese_Brasil");
     //Define a cor azul do terminal
     system("color 1F");
     //define o titulo da janela do prompt
     system("title Menu");
 
-    int r;
+    printf("\e[H\e[2J"); //Limpa a tela
+
+    int resposta;
     printf("\n");
     printf("\n ==============================================");
-    printf("\n ============Controle de Pizzaria==============");
+    printf("\n ============ Controle de Pizzaria ==============");
     printf("\n ==============================================");
     printf("\n");
     printf(" Opcoes disponiveis:");
@@ -59,9 +283,9 @@ int main()
     printf(" (1) Funcionarios \n (2) Clientes \n (3) Produtos \n (4) Ingredientes \n (5) Contabilizacao \n (6) Esqueci esse \n (7) Esse tambem \n (8) Ralatorios \n (9) Fechamento  \n (0) Sair ");
     printf("\n \n \n Selecione uma das opcoes acima: \n");
 
-    scanf("%d", &r);
+    scanf("%d", &resposta);
 
-    switch (r){
+    switch (resposta){
         
     case 1:
         showFuncionarios();
@@ -104,9 +328,7 @@ int main()
         return 0;
         break;
     }
-    
 
     printf("\n");
 
-    main();
 }

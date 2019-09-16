4ef84a1344dfbf1c29ff224131bf2505ba848a4e
José Hugo <jose.barros36@etec.sp.gov.br>
2019-09-15 22:18:27 -0300

    Feito a criação do arquivo temporario para que guarde o usuário que logar no sistema

--- Usuario/login.c
+++ Usuario/login.c
@@ -9,14 +9,16 @@ Autor: Hugo Barros
 Data de criação 15/09/2019
 Descrição:
 Implementação 01: Feito a criação da função (LerArquivoUsuario),O mesmo vai ler o arquivo texto com o nome do usuário caso esteja certo usuário e senha o login será feito
+Implementação 02: Feito a criação do arquivo temporario para que guarde o usuário que logar no sistema
 */
 
 void LerArquivoUsuario(){
 
     char usuario[50],senha[10],senhareal[10];
     FILE *ArqUsr;
+    FILE *UsuarioLogado;
 
-    printf("Digite seu nome de usuário: ");gets(usuario);//Usuário digita o nome com que quer se logar
+    printf("Digite seu nome de usuario: ");gets(usuario);//Usuário digita o nome com que quer se logar
     ArqUsr = fopen(usuario,"r");//abre o arquivo com o nome do usuário
     printf("Digite sua senha: ");gets(senha);//usuário informa sua senha
     
@@ -25,18 +27,21 @@ void LerArquivoUsuario(){
                 if (strcmp(senha,senhareal)!=0)//e a senha informada pelo usuário bater com a senha real...
                 {
                     printf("Login feito com sucesso!");//é informado ao usuário que ele conseguiu se logar
-                    system("cd..");
-                     system("cd menu");
+                    //system("cd..");
+                    //system("cd menu");
+                    UsuarioLogado =fopen("Usuario_Logado.txt","a");
+                    fprintf(UsuarioLogado,"%s",usuario);//grava no arquivo usuário Usuário que logou
+                    fclose(UsuarioLogado);//fecha o arquivo
                     //break;
                 }
                 else{//se não...
                         
-                        printf("Usuário ou senha não está correto. Tente novamente...");//é informado que os dados estão incorretos
+                    printf("Usuario ou senha nao estao correto. Tente novamente...");//é informado que os dados estão incorretos
                         //break;
                 }                                
         }
         else{
-            printf("Usuário ou senha não está correto. Tente novamente...");//quando o usuário tenta entrar com um nome inválido
+            printf("Usuario nao cadastrado...");//quando o usuário tenta entrar com um nome inválido
             //break;
         }
 }

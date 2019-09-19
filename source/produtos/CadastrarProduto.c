88a45b0b10a918d202eed7c66205c12f74319f9b
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-09-19 00:28:23 -0300

    Header para mover arquivos para ./data/

--- source/produtos/CadastrarProduto.c
+++ source/produtos/CadastrarProduto.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-//#include <windows.h>
+/* #include <conio.h> */
 #include <locale.h>
 
-int main(){
+void CadastrarProduto(){
+    
+}
 
+void main()
+{
     //Define o padrão UTF-8
     setlocale(LC_ALL, "Portuguese_Brasil");
     //Define a cor azul do terminal
-    /* system("color 1F"); */
+    system("color 1F");
     //define o titulo da janela do prompt
-
-    system("./Usuario/login");
+    system("title Menu");
+    LerArquivoUsuario();
 }

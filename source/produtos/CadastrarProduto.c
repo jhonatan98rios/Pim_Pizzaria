7b386e75da65c270774be4dadcdf42a5712e3df5
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-10-01 00:33:22 -0300

    Product check-in

--- source/produtos/CadastrarProduto.c
+++ source/produtos/CadastrarProduto.c
@@ -5,44 +5,107 @@
 #include <math.h>
 /* #include <conio.h> */
 
-struct produto{
+struct produtos{
+    struct pizzas{
+        int id;
         char nome[10];
         float preco;
+    } pizzas;
+
+    struct bebidas{
+        int id;
+        char nome[10];
+        float preco;
+        int quantidade;
+    } bebidas;
 };
 
 void LerProdutos(){
     FILE *infile;
-    struct produto input;
+    struct produtos input;
     infile = fopen ("produtos.dat", "r");
-
-    while(fread(&input, sizeof(struct produto), 1, infile))
-    
-    printf ("nome = %s preco = %f\n", input.nome, input.preco);
-
+    printf ("Pizzas \n");
+    while(fread(&input, sizeof(struct produtos), 1, infile)){
+        printf ("nome = %s \npreco = %.2f\n", input.pizzas.nome, input.pizzas.preco);
+        printf ("nome = %s \npreco = %.2f\n", input.bebidas.nome, input.bebidas.preco);
+    };
     fclose (infile);
-}
-
+};
 
 void CadastrarProduto(){
 
     FILE *ProdFile;
-    ProdFile = fopen("produtos.dat", "w");
+    ProdFile = fopen("produtos.dat", "a");
 
-    struct produto prod;
+    struct produtos prod;
+    int resp;
 
-    printf("Informe o nome do produto: ");
-    scanf("%s", prod.nome);
-    printf("Informe o preco: ");
-    scanf("%f", &prod.preco);
+    printf("Informe o que deseja cadastrar: \n \n [1] Pizzas \n [2] Bebidas \n");
+    scanf("%d", &resp );
 
-    fwrite (&prod, sizeof(struct produto), 1, ProdFile);
+    switch (resp)
+    {
+    case 1:
+        printf("\e[1;1H\e[2J");
+        printf("Informe o nome da pizza: \n");
+        scanf("%s", prod.pizzas.nome);
+        printf("Informe o preco da pizza: \n");
+        scanf("%f", &prod.pizzas.preco);
+        break;
+    
+    default:
+        printf("\e[1;1H\e[2J");
+        printf("Informe o nome da bebida: \n");
+        scanf("%s", prod.bebidas.nome);
+        printf("Informe o preco da bebida: \n");
+        scanf("%f", &prod.bebidas.preco);
+        break;
+    }
 
-    if(&fwrite != 0)  
+    fwrite (&prod, sizeof(struct produtos), 1, ProdFile);
+
+    if(&fwrite != 0){
+        printf("\e[1;1H\e[2J");
         printf("Salvo com sucesso !\n"); 
-    else 
+    }else{
+        printf("\e[1;1H\e[2J"); 
         printf("Erro ao gravar !\n");
-
+    }
+    
     fclose(ProdFile);
+}
+
+void menu(){
+    int resp;
+    int repeat = 1;
+
+    while(repeat == 1){
+        printf("O que deseja fazer? \n\t[1] Cadastrar \n\t[2] Vizualizar \n");
+        scanf("%d", &resp);
+
+        switch (resp)
+        {
+        case 1:
+            printf("\e[1;1H\e[2J");
+            CadastrarProduto();
+            break;
+
+        case 2:
+            printf("\e[1;1H\e[2J");
+            LerProdutos();
+            break;
+        
+        default:
+            printf("\e[1;1H\e[2J");
+            printf("Essa opcao nao existe seu burro");
+            printf("O que deseja fazer? \n\t[1] Cadastrar \n\t[2] Vizualizar \n");
+
+        }
+
+        printf("Deseja continuar? \n\t [1] Sim \n\t [2] Nao \n");
+        scanf("%d", &repeat);
+        printf("\e[1;1H\e[2J");
+    }
     
 }
 
@@ -55,9 +118,8 @@ int main()
     //define o titulo da janela do prompt
     //system("title Menu");
 
-    //CadastrarProduto();
-
-    LerProdutos();
+    printf("\e[1;1H\e[2J");
+    menu();
 
     return 0;
 }

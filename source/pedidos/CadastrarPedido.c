15f26910c7854fcd3b226a8f87e409d542332cf9
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-10-15 23:32:37 -0300

    Cadastro de pedidos

--- source/pedidos/CadastrarPedido.c
+++ source/pedidos/CadastrarPedido.c
@@ -12,13 +12,13 @@ struct produtos{
 void LerProd( char titulo[10], int category ){ /* Isso deve virar uma funcao de cabeçalho */
 
     FILE *infile;
-    struct produtos prod;
-    infile = fopen ("../../data/produtos.dat", "r");
+    struct produtos prod; /* Cria uma instância para vizualização */
+    infile = fopen ("../../data/produtos.dat", "r"); /* Salva os produtos armazenados em memória */
 
     printf("\n =================== %s =================== \n", titulo);
     while(fread(&prod, sizeof(struct produtos), 1, infile)){ /* Isso deve virar uma funcao de cabeçalho */
         if(prod.category == category){
-            printf ("\n nome = %s \n preco = %.2f\n", prod.name, prod.price);
+            printf ("\n nome = %s \n preco = %.2f\n", prod.name, prod.price); /* Percorre o array printando */
         }
     };
 
@@ -27,17 +27,21 @@ void LerProd( char titulo[10], int category ){ /* Isso deve virar uma funcao de 
 
 void CadastrarPedido(){
 
-    int category = 0;
     char produto[10];
+    int confirm = 0;
 
     FILE *infile;
-    struct produtos prod;
-    infile = fopen ("../../data/produtos.dat", "r");
+    infile = fopen("../../data/produtos.dat", "r"); /* Abre o arquivo para leitura */
+    struct produtos prod; /* prod é o objeto vindo da memoria */
+
+    FILE *InputFile; /* Cria um arquivo para armazenar a compra */
+    InputFile = fopen("./data/carrinho_de_compra.dat", "a"); /* Abre o arquivo para adição */
+    struct produtos input; /* input é o objeto informado */
 
     printf("O que deseja comprar? \n\n [1] Pizzas \n [2] Bebidas \n [3] Doces \n");
-    scanf("%d", &category );
+    scanf("%d", &input.category );
 
-    switch(category)
+    switch(input.category)
     {
     case 1:
         LerProd( "Pizzas" , 1);
@@ -54,32 +58,69 @@ void CadastrarPedido(){
         break;
     }
 
-    printf(" \n Informe o que nome do produto: \n");
-    scanf("%s", produto);
+    printf(" \n Informe o nome do produto: \n");
+    scanf("%s", input.name);
     
-    int compare =  strcmp("Brigadeiro", produto); /* Preciso verificar se produto é igual à a prod.name */
+    int compare =  strcmp(prod.name, input.name); /* Verifica se produto é igual à a prod.name */
+    int produtoValido = 0;
 
     while(fread(&prod, sizeof(struct produtos), 1, infile)){
 
-        size_t length = sizeof(produto)/sizeof(produto[0]); /* Isso deve virar uma funcao de cabeçalho */
+        if(prod.category == input.category){
 
-        if(prod.category == category){
-
-            printf("%d \n", compare);
-            printf("%s \n", produto);
+            printf("%s \n", input.name);
             printf("%s \n", prod.name);
+            printf("%d \n", compare);
 
+/* ======================================================   */
             if( compare == 0 ){
                 printf ("\n nome = %s \n preco = %.2f\n", prod.name, prod.price);
+                produtoValido = 1;
+                input.price = prod.price; /* Salva o valor do produto na struct input */
+                break;
             }
+/* ======================================================   */
         }
     };
 
-    if(compare != 0){
+    if(produtoValido != 0){
         printf("Produto Inválido \n");
         CadastrarPedido();
-    }
+    };
 
+    printf("Confirmar pedido?");
+    scanf("%d", &confirm);
+
+    if(confirm == 0){
+        printf("Compra cancelada!");
+        CadastrarPedido();
+        exit(0);
+
+    }else{
+        confirm = 0;
+        fwrite (&input, sizeof(struct produtos), 1, ProdFile); /* Grava o produto no carrinho */
+
+        if(&fwrite != 0){ /* Se conseguir gravar */
+            printf("\e[1;1H\e[2J");
+            printf("Cadastrado com sucesso !\n\n"); 
+        }else{
+            printf("\e[1;1H\e[2J"); 
+            printf("Erro ao gravar !\n");
+            system( "read -n 1 -s -p \"Press any key to continue...\"" ); /* Pause */
+        }
+
+        printf("Deseja adicionar produtos? \n [1] Sim - [0] Não");
+        scanf("%d", &confirm);
+
+        if(confirm == 1){
+            CadastrarPedido();
+            exit(0);
+        }else{
+
+
+
+        }
+    }
 }
 
 int main(){

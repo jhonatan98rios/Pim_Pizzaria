20c45915e06291748394d4a0f69f15fff2df88fc
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-10-21 00:02:50 -0200

    Store module ready

---
+++ source/estoque/VizualizarEstoque.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <math.h>
+/* #include <conio.h> */
+#include "./LerEst.h"
+
+void LerEstoque(){
+
+    printf("\n ==============================================");
+    printf("\n ================= Estoque ===================");
+    printf("\n ==============================================\n");
+
+    LerEst();
+    
+    printf("\n\n");
+};
+
+int main()
+{
+    //Define o padrão UTF-8
+    setlocale(LC_ALL, "Portuguese_Brasil");
+    //Define a cor azul do terminal
+    //system("color 1F");
+    //define o titulo da janela do prompt
+    //system("title Menu");
+
+    printf("\e[1;1H\e[2J");
+    LerEstoque();
+
+    return 0;
+}

24aa482258e3956953ae05ca1cf5f0f2bf753b93
Jhonatan Maia Rios <jhoatan98rios@gmail.com>
2019-10-21 01:20:28 -0200

    Bugfix

--- source/estoque/VizualizarEstoque.c
+++ source/estoque/VizualizarEstoque.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <locale.h>
 #include <math.h>
-#include <unistd.h>
 /* #include <conio.h> */
 #include "../../libs/LerEst.h"
 
@@ -29,11 +28,6 @@ int main()
 
     printf("\e[1;1H\e[2J");
     LerEstoque();
-
-    printf("teste");
-
-    int teste;
-    scanf("%d", &teste);
 
     return 0;
 }

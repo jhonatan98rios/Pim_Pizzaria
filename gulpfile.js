4f49c17449b3a72986979328bd46fe215d98edf6
jhonatan.rios <jhonatan.rios@the9level.com>
2019-10-31 14:42:44 -0300

    Compile automatic

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,27 @@ const exec = require('child_process').exec;
 
 const builds = [
     "sudo gcc ./index.c -o ./index",
-    "sudo gcc ./source/menu.c -o ./source/menu",
-    "sudo gcc ./source/produtos/MenuProduto.c -o ./source/produtos/MenuProduto",
-    "sudo gcc ./source/produtos/CadastrarProduto.c -o ./source/produtos/CadastrarProduto",
-    "sudo gcc ./source/produtos/VizualizarProduto.c -o ./source/produtos/VizualizarProduto",
-    "sudo gcc ./source/pedidos/MenuPedidos.c -o ./source/pedidos/MenuPedidos",
-    "sudo gcc ./source/pedidos/CadastrarPedido.c -o ./source/produtos/CadastrarPedido",
+    "sudo gcc ./source/menu.c -o dist/menu",
+    "sudo gcc ./source/produtos/MenuProdutos.c -o dist/produtos/MenuProdutos",
+    "sudo gcc ./source/produtos/CadastrarProduto.c -o dist/produtos/CadastrarProduto",
+    "sudo gcc ./source/produtos/VisualizarProduto.c -o dist/produtos/VisualizarProduto",
 ];
 
-for(let i = 0; i < 4; i++){
+
+gulp.task('default', async function(){
+    builds.forEach((build)=>{
+        exec(build, function(stdout, stderr){
+            console.log(stdout);
+            console.log(stderr);
+        });
+    });
+});
+
+/* for(let i = 0; i < 4; i++){
     gulp.task('default', async function(){
         exec(builds[i], function(error, stdout, stderr){
             console.log(stdout);
             console.log(stderr);
         });
     });
-};
+}; */
